1e79e5ef62cb3dd9794454b487a207e214134f13
seojin1217 <dalmurii89@gmail.com>
2025-07-08 09:47:22 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log("후츠릿 구독 좋아요");
+// 자바스크립트에서 주석은 ctrl + 슬래쉬
+
+const youtube = "후츠릿";
+// 유투브라는 상수에 후츠릿이란 값을 집어넣겠다는 뜻. 하나의 변수는 같은 자바안에서 고유해야함
+
+let comment = "구독 좋아요";
+console.log("🚀 ~ comment:", comment);
+// let 은 변수, const 는 상수. 상수는 재할당이 불가, 변수는 가능
+
+console.log("youtube", youtube);
+comment = "태요미네";
+// 이미 정의되어 있는 구독 좋아요를 태요미네로 재할당하겠다는 뜻.
+console.log("🚀 ~ comment:", comment);
+// 변경 이후부터 실행된 콘솔로그부터 변경값으로 사용됨. 순차적으로 사용됨.
